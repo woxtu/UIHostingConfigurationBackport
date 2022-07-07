a4e0386fccb717f0102b6c9d44618c558dedcf37
woxtu <woxtup@gmail.com>
2022-07-08 06:03:14 +0900

    Set the margins

--- Sources/UIHostingConfigurationBackport/UIHostingConfigurationBackport.swift
+++ Sources/UIHostingConfigurationBackport/UIHostingConfigurationBackport.swift
@@ -4,15 +4,18 @@ import UIKit
 public struct UIHostingConfigurationBackport<Content, Background>: UIContentConfiguration where Content: View, Background: View {
   let content: Content
   let background: Background
+  let margins: NSDirectionalEdgeInsets
 
   public init(@ViewBuilder content: () -> Content) where Background == EmptyView {
     self.content = content()
     background = .init()
+    margins = .zero
   }
 
-  init(content: Content, background: Background) {
+  init(content: Content, background: Background, margins: NSDirectionalEdgeInsets) {
     self.content = content
     self.background = background
+    self.margins = margins
   }
 
   public func makeContentView() -> UIView & UIContentView {
@@ -26,14 +29,37 @@ public struct UIHostingConfigurationBackport<Content, Background>: UIContentConf
   public func background<S>(_ style: S) -> UIHostingConfigurationBackport<Content, _UIHostingConfigurationBackgroundViewBackport<S>> where S: ShapeStyle {
     return UIHostingConfigurationBackport<Content, _UIHostingConfigurationBackgroundViewBackport<S>>(
       content: content,
-      background: .init(style: style)
+      background: .init(style: style),
+      margins: margins
     )
   }
 
   public func background<B>(@ViewBuilder content: () -> B) -> UIHostingConfigurationBackport<Content, B> where B: View {
     return UIHostingConfigurationBackport<Content, B>(
       content: self.content,
-      background: content()
+      background: content(),
+      margins: margins
+    )
+  }
+
+  public func margins(_ insets: EdgeInsets) -> UIHostingConfigurationBackport<Content, Background> {
+    return UIHostingConfigurationBackport<Content, Background>(
+      content: content,
+      background: background,
+      margins: .init(insets)
+    )
+  }
+
+  public func margins(_ edges: Edge.Set = .all, _ length: CGFloat) -> UIHostingConfigurationBackport<Content, Background> {
+    return UIHostingConfigurationBackport<Content, Background>(
+      content: content,
+      background: background,
+      margins: .init(
+        top: edges.contains(.top) ? length : margins.top,
+        leading: edges.contains(.leading) ? length : margins.leading,
+        bottom: edges.contains(.bottom) ? length : margins.bottom,
+        trailing: edges.contains(.trailing) ? length : margins.trailing
+      )
     )
   }
 }
@@ -53,6 +79,7 @@ final class UIHostingContentViewBackport<Content, Background>: UIView, UIContent
           configuration.background
           configuration.content
         }
+        directionalLayoutMargins = configuration.margins
       }
     }
   }
@@ -64,10 +91,10 @@ final class UIHostingContentViewBackport<Content, Background>: UIView, UIContent
 
     addSubview(hostingController.view)
     NSLayoutConstraint.activate([
-      hostingController.view.topAnchor.constraint(equalTo: topAnchor),
-      hostingController.view.leadingAnchor.constraint(equalTo: leadingAnchor),
-      hostingController.view.bottomAnchor.constraint(equalTo: bottomAnchor),
-      hostingController.view.trailingAnchor.constraint(equalTo: trailingAnchor),
+      hostingController.view.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+      hostingController.view.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+      hostingController.view.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+      hostingController.view.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
     ])
   }
 
